84b35892fd87b7e0056fd9dc59ce3e266ac714aa
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-07-13 22:41:21 +0200

    Added more tests

--- src/test/kotlin/ScriptFileTest.kt
+++ src/test/kotlin/ScriptFileTest.kt
@@ -17,12 +17,77 @@ class ScriptFileTest {
             main
         """.trimIndent()
 
-        val output = CommentStripper.strip(input.split("\n")).joinToString()
-
+        val output = CommentStripper.strip(input.split("\n"))
+            .joinToString(separator = "\n")
         assertEquals(output, exceptedOutput);
-
 
     }
 
+    @Test
+    fun stripCommentsTest2() {
 
+        val input = """
+        
+            // other comment type
+            // with blank lines
+            
+            main
+        
+        """.trimIndent()
+
+        val exceptedOutput = """
+            main
+        """.trimIndent()
+
+        val output = CommentStripper.strip(input.split("\n"))
+            .joinToString(separator = "\n")
+        assertEquals(output, exceptedOutput);
+
+    }
+
+    @Test
+    fun stripCommentsTest3() {
+
+        val input = """
+             // intends
+              # with the
+               // comments
+                # which looks
+                 # really cool
+                  // this should work quite deep
+            main
+        """.trimIndent()
+
+        val exceptedOutput = """
+            main
+        """.trimIndent()
+
+        val output = CommentStripper.strip(input.split("\n"))
+            .joinToString(separator = "\n")
+        assertEquals(output, exceptedOutput);
+
+    }
+
+    @Test
+    fun stripCommentsTest4() {
+
+        val input = """
+            main
+             println
+              Hello World // comments will be printed
+              // here not
+        """.trimIndent()
+
+        val exceptedOutput = """
+            main
+             println
+              Hello World // comments will be printed
+        """.trimIndent()
+
+        val output = CommentStripper.strip(input.split("\n"))
+            .joinToString(separator = "\n")
+
+        assertEquals(output, exceptedOutput);
+
+    }
 }
