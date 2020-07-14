27cb781bfef148f857526368a5a20880fca899b7
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-07-14 10:17:46 +0200

    Removed deprecated method

--- src/main/kotlin/eu/gebes/utils/FileParsing.kt
+++ src/main/kotlin/eu/gebes/utils/FileParsing.kt
@@ -1,9 +1,5 @@
 package eu.gebes.utils
 
-@Deprecated("Use trimIntend of string instead", ReplaceWith("line.trimIntend()"))
-fun stripIntend(line: String): String {
-    return line.substring(getIntend(line), line.length)
-}
 
 fun getIntend(line: String): Int {
 
@@ -13,7 +9,6 @@ fun getIntend(line: String): Int {
         if (char == ' ' && count != 2)
             count++
         else return count
-
 
     return count
 }
